2ca9a76f32a14ba5d4a577fe98085115b1788f7d
davidjohanssonbf <89454414+davidjohanssonbf@users.noreply.github.com>
2023-10-15 17:14:40 +0200

    ..

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,7 @@ import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient, withJsonpSupport } from '@angular/common/http';
 import { MAT_ICON_DEFAULT_OPTIONS } from '@angular/material/icon';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
 	providers: [
@@ -12,6 +13,10 @@ export const appConfig: ApplicationConfig = {
 		provideHttpClient(
 			withJsonpSupport()
 		),
+		{
+			provide: LocationStrategy,
+			useClass: HashLocationStrategy,
+		},
 		{
 			provide: LOCALE_ID,
 			useValue: 'sv-SE',
